79037fa0c034439a619668cb98fc3c0629524dad
Andrew Nguyen <andrewnguyen841@gmail.com>
2022-05-16 22:06:03 -0700

    remove unused code, refactor TriviaMaze.java

--- src/model/TriviaMaze.java
+++ src/model/TriviaMaze.java
@@ -1,37 +1,34 @@
 package model;
 
-import model.*;
+import model.mazecomponents.*;
+import model.questions.Question;
+
+import java.util.Scanner;
+
 public class TriviaMaze {
 
     /**
      * The maze.
      */
-    private Maze myMaze;
+    private static Maze myMaze;
+
+
     public static void main(final String[] theArgs) {
         myMaze = new Maze(10, 10);
         // introMessage();
         do {
-            System.out.println(myMaze.toString);
+            System.out.println(myMaze.toString());
             Scanner input = new Scanner(System.in);
             System.out.print("Choose a direction to move in (E, W, N, S): ");
             String direction = input.nextLine();
-            Boolean moved = false;
-            switch (direction.toUpperCase()) {
-                case "E":
-                    // Turn into method
-                    moved = movePlayer(this.myMaze, Direction.EAST);
-                    break;
-                case "W":
-                    moved = movePlayer(this.myMaze, Direction.EAST);
-                    break;
-                case "N":
-                    moved = movePlayer(this.myMaze, Direction.EAST);
-                    break;
-                case "S":
-                    moved = movePlayer(this.myMaze, Direction.EAST);
-                    break;
-            }
-        } while (myMaze.atGoal!)
+            boolean moved = switch (direction.toUpperCase()) {
+                case "E" -> movePlayer(myMaze, Direction.EAST, input);
+                case "W" -> movePlayer(myMaze, Direction.WEST, input);
+                case "N" -> movePlayer(myMaze, Direction.NORTH, input);
+                case "S" -> movePlayer(myMaze, Direction.SOUTH, input);
+                default -> false;
+            };
+        } while (!myMaze.atGoal());
     }
 
     private static boolean movePlayer(Maze theMaze, final Direction theDirection, final Scanner theInput) {
@@ -39,16 +36,16 @@ public class TriviaMaze {
         System.out.println(question.toString());
         System.out.print("Select an option (A, B, C, D): ");
         String selection = theInput.nextLine();
-        Boolean correctness = question.checkAnswer(selection);
+        boolean correctness = question.checkAnswer(selection);
         if (correctness) {
             myMaze.changeDoorState(theDirection, State.OPEN);
-            moved = myMaze.move(theDirection);
-            if (moved!)
-            throw new Exception("Player failed to move.");
+            boolean moved = myMaze.move(theDirection);
+//            if (!moved) {
+//                throw new Exception("Player failed to move.");
+//            }
         } else {
-            return false
+            return false;
         }
         return true;
-
     }
 }
