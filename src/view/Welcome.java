f5bc9805375f07465998558c86c72d2e6e6bfb57
Timon <ttukei@uw.edu>
2022-05-21 02:11:42 -0700

    Began implementation of the welcome screen gui

---
+++ src/view/Welcome.java
@@ -0,0 +1,20 @@
+package view;
+
+import javax.swing.*;
+
+public class Welcome {
+
+    private JFrame myFrame;
+    private JPanel myPanel;
+    private ImageIcon myImageIcon;
+    private JLabel myLabel;
+
+
+    public Welcome() {
+
+    }
+    private static void main(String[] args) {
+
+    }
+
+}
