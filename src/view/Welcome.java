39aa0ed6578b22b58a93d84f5812ea67859fb0ae
Timon <ttukei@uw.edu>
2022-05-21 03:07:37 -0700

    Began implementation of the welcome screen gui

--- src/view/Welcome.java
+++ src/view/Welcome.java
@@ -1,16 +1,34 @@
 package view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class Welcome {
 
     private JFrame myFrame;
     private JPanel myPanel;
-    private ImageIcon myImageIcon;
+    private Image myImage;
     private JLabel myLabel;
+    private final int myFrameWidth = 500, myFrameHeight = 500;
+    private final String myBackground = "assets/Landing_Page_01.png";
 
 
     public Welcome() {
+        myFrame = new JFrame();
+        myFrame.setTitle("Trivia Maze");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setResizable(false);
+        myFrame.setSize(myFrameWidth, myFrameHeight);
+        myFrame.setVisible(true);
+
+        myImage = Toolkit.getDefaultToolkit().getImage(myBackground);
+        myPanel = new JPanel();
+        myPanel.setSize(myFrameWidth, myFrameHeight);
+        
+
 
     }
     private static void main(String[] args) {
