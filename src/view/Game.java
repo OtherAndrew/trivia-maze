1998526b1de2dabe6365c582d190d892aa30b8a5
Timon <ttukei@uw.edu>
2022-05-26 12:45:22 -0700

    Worked on design for game.java and began implementation

--- src/view/Game.java
+++ src/view/Game.java
@@ -2,34 +2,61 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class Game {
 
     private JFrame myFrame;
-    private JButton myRoomButton, myMapButton;
+    private JPanel myMapPanel, myDirectionPanel, myQuestionPanel, myAnswerPanle;
+    private JButton myRoomButton, myMapButton, myEastButton, myWestButton, myNorthButton, mySouthButton;
     private final String myTitle = "Trivia Maze", myWindowIconPath = "assets\\Landing_page_01.png";
     private final Dimension myPreferredSize = new Dimension(1000,1000);
 
     public Game() {
         myFrame = new JFrame(myTitle);
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.setLayout(new GridLayout(10, 10));
+        myFrame.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
         myFrame.setSize(myPreferredSize);
         myFrame.setLocationRelativeTo(null);
+
+
+        // Top left
+        myMapPanel = new JPanel();
+        // TODO Show current room and highlight doors
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.ipadx = 250;
+        gbc.gridy = 0;
+        gbc.gridx = 0;
+        myFrame.add(myMapPanel);
+
+        // Lower left
+        myDirectionPanel = new JPanel(new GridLayout(1, 4));
+        myEastButton = new JButton("East");
+        myNorthButton = new JButton("North");
+        mySouthButton = new JButton("South");
+        myWestButton = new JButton("West");
+        // TODO Add action listeners to buttons
+        myDirectionPanel.add(myEastButton);
+        myDirectionPanel.add(myNorthButton);
+        myDirectionPanel.add(mySouthButton);
+        myDirectionPanel.add(myWestButton);
+        gbc.gridy = 1;
+        gbc.gridx = 0;
+        myFrame.add(myDirectionPanel);
+
+        // Top right
+
+        // Lower right
+
+
+
         myFrame.setVisible(true);
 
-//        for (int row = 0; row < maze.length; row++) {
-//            for (int col = 0; col < maze[0].length; col++) {
-//                JButton tile = makeTile(maze[row][col]);
-//                mainFrame.add(tile);
-//            }
-//        }
+
+
+
     }
 
-//    private JButton makeTile() {
-//
-//    }
     public static void main(String[] args) {
         Game game = new Game();
     }
