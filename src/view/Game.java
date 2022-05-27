d4aa653aea3ab4ad581e4f292dbc9fb5d3f2cc80
Timon <ttukei@uw.edu>
2022-05-27 14:13:26 -0700

    Worked on game.java implementation

--- src/view/Game.java
+++ src/view/Game.java
@@ -6,44 +6,27 @@ import java.awt.*;
 public class Game {
 
     private JFrame myFrame;
-    private JPanel myMapPanel, myDirectionPanel, myQuestionPanel, myAnswerPanel;
+    private JPanel myMapPanel, myDirectionPanel, myQuestionAnswerPanel;
     private JButton myRoomButton, myMapButton, myEastButton, myWestButton, myNorthButton, mySouthButton;
     private JLabel myQuestion;
+    private JRadioButton[] myAnswerButtons;
+    private ButtonGroup myAnswerButtonsGroup;
     private final String myTitle = "Trivia Maze", myWindowIconPath = "assets\\Landing_page_01.png";
     private final Dimension myPreferredSize = new Dimension(1000,1000);
 
     public Game() {
         myFrame = new JFrame(myTitle);
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
+        myFrame.setLayout(new BorderLayout());
         myFrame.setSize(myPreferredSize);
         myFrame.setLocationRelativeTo(null);
 
-
-        // Top left
-        myMapPanel = new JPanel();
-        myMapPanel.setBackground(new Color(255, 44,56));
+        // Center
+        myMapPanel = new JPanel(new BorderLayout());
+        myMapPanel.setBackground(new Color(0, 0,0));
         // TODO Show current room and highlight doors
 
-        gbc.ipady = 500;
-        gbc.ipadx = myFrame.getWidth()/2;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        myFrame.add(myMapPanel, gbc);
-        gbc.ipady = 0;
-        gbc.ipadx = 0;
-
-        // Top right
-        myQuestionPanel = new JPanel();
-
-        myQuestion = new JLabel("The question"); // Get the question corresponding to selected door and display it
-        myQuestionPanel.add(myQuestion, BorderLayout.CENTER);
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        myFrame.add(myQuestionPanel, gbc);
-
-        // Lower left
+        // Bottom
         myDirectionPanel = new JPanel(new GridLayout(1, 4));
         myEastButton = new JButton("East");
         myNorthButton = new JButton("North");
@@ -54,20 +37,34 @@ public class Game {
         myDirectionPanel.add(myNorthButton);
         myDirectionPanel.add(mySouthButton);
         myDirectionPanel.add(myWestButton);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        myFrame.add(myDirectionPanel, gbc);
+        myMapPanel.add(myDirectionPanel, BorderLayout.SOUTH);
 
-        // Lower right
+        myFrame.add(myMapPanel, BorderLayout.CENTER);
 
+        // Top Right
+        myQuestionAnswerPanel = new JPanel(new GridLayout(2, 1));
+
+        // TODO Get the question corresponding to selected door and display it
+        myQuestion = new JLabel("The question");
+
+        myQuestionAnswerPanel.add(myQuestion, BorderLayout.CENTER);
+
+        // Bottom Right
+        int numberOfAnswers = 4; // TODO get the number of answers from controller
+        myAnswerButtons = new JRadioButton[numberOfAnswers];
+        myAnswerButtonsGroup = new ButtonGroup();
+        for (int i = 0; i < numberOfAnswers; i++) {
+            myAnswerButtons[i] = new JRadioButton(Integer.toString(i));
+            myAnswerButtonsGroup.add(myAnswerButtons[i]);
+        }
+        for (int i = 0; i < numberOfAnswers; i++) {
+        myQuestionAnswerPanel.add(myAnswerButtons[i]);
+        }
+        myFrame.add(myQuestionAnswerPanel, BorderLayout.EAST);
 
 
 
         myFrame.setVisible(true);
-
-
-
-
     }
 
     public static void main(String[] args) {
