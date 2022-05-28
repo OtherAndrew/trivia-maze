05265b6029d1cefaac7e8034a69d22a6d65eab75
Andrew Nguyen <andrewnguyen841@gmail.com>
2022-05-28 14:48:51 -0700

    extract methods to create panels

--- src/view/Game.java
+++ src/view/Game.java
@@ -6,10 +6,6 @@ import model.mazecomponents.Room;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Scanner;
 
 public class Game {
 
@@ -30,18 +26,77 @@ public class Game {
         myFrame.setSize(myPreferredSize);
         myFrame.setLocationRelativeTo(null);
 
-        // Center
-        myMapPanel = new JPanel(new BorderLayout());
-        myMapPanel.setBackground(new Color(0, 0,0));
         // TODO Show current room and highlight doors
 
-        JPanel controls = new JPanel(new BorderLayout());
-
         // Left half
-
-        // Top Left
-        // TODO: map display
 //        final String mazePlaceholder = Files.readString(Path.of("src/view/assets/mazePlaceholder.txt"));
+        myFrame.add(drawMapDisplay(), BorderLayout.CENTER);
+
+        // Right
+        JPanel sidebar = new JPanel(new BorderLayout());
+        sidebar.add(drawDirectionControls(), BorderLayout.SOUTH);
+        sidebar.add(drawQAPanel(), BorderLayout.CENTER);
+        myFrame.add(sidebar, BorderLayout.EAST);
+
+        myFrame.setVisible(true);
+    }
+
+    private JPanel drawQAPanel() {
+        // Top
+        myQuestionAnswerPanel = new JPanel(new BorderLayout());
+
+        // TODO Get the question corresponding to selected door and display it
+        myQuestion = new JTextArea();
+        myQuestion.setLineWrap(true);
+        myQuestion.setText("Cat's Paw heels, manufactured by Biltrite and first available in 1904, are instantly recognizable on vintage footwear due to their bright white no-slip rubber dots and asymmetrical semi-circles along the edges of the heels creating the Cat's Paw shape. Even more recognizable is this iconic American brand's bright red packaging for the heels that featured a black cat on the side.");
+        myQuestionAnswerPanel.add(myQuestion, BorderLayout.CENTER);
+
+        // Bottom
+        myQuestionAnswerPanel.add(drawAnswerPanel(), BorderLayout.SOUTH);
+        return myQuestionAnswerPanel;
+    }
+
+    private JPanel drawAnswerPanel() {
+        final String[] answerArray = {"Augustus Ludlow", "Shinki Hikaku", "John Lofgren", "Wabash"};
+
+        int numberOfAnswers = answerArray.length; // TODO get the number of answers from controller
+        myAnswerPanel = new JPanel(new GridLayout(numberOfAnswers + 2, 1));
+        myAnswerButtons = new JRadioButton[numberOfAnswers];
+        myAnswerButtonsGroup = new ButtonGroup();
+        for (int i = 0; i < numberOfAnswers; i++) {
+            myAnswerButtons[i] = new JRadioButton(answerArray[i]);
+            myAnswerButtonsGroup.add(myAnswerButtons[i]);
+        }
+        for (int i = 0; i < numberOfAnswers; i++) {
+            myAnswerPanel.add(myAnswerButtons[i]);
+        }
+        myAnswerPanel.add(new JButton("Submit"));
+        JPanel buffer = new JPanel();
+        myAnswerPanel.add(buffer);
+        return myAnswerPanel;
+    }
+
+    private JPanel drawDirectionControls() {
+        myDirectionPanel = new JPanel(new GridLayout(3, 3));
+        myEastButton = new JButton("Right");
+        myNorthButton = new JButton("Up");
+        mySouthButton = new JButton("Down");
+        myWestButton = new JButton("Left");
+        // TODO Add action listeners to buttons
+
+        myDirectionPanel.add(new JPanel());
+        myDirectionPanel.add(myNorthButton);
+        myDirectionPanel.add(new JPanel());
+        myDirectionPanel.add(myWestButton);
+        myDirectionPanel.add(new JPanel());
+        myDirectionPanel.add(myEastButton);
+        myDirectionPanel.add(new JPanel());
+        myDirectionPanel.add(mySouthButton);
+        myDirectionPanel.add(new JPanel());
+        return myDirectionPanel;
+    }
+
+    private JPanel drawMapDisplay() {
         final Maze maze = new Maze(5, 5);
         System.out.println(maze);
         final char[][] mazeChar = maze.toCharArray();
@@ -66,72 +121,7 @@ public class Game {
                 myMapDisplay.add(tile);
             }
         }
-
-
-        myFrame.add(myMapDisplay, BorderLayout.CENTER);
-
-
-        // Bottom Left
-        myDirectionPanel = new JPanel(new GridLayout(3, 3));
-        myEastButton = new JButton("Right");
-        myNorthButton = new JButton("Up");
-        mySouthButton = new JButton("Down");
-        myWestButton = new JButton("Left");
-        // TODO Add action listeners to buttons
-
-        myDirectionPanel.add(new JPanel());
-        myDirectionPanel.add(myNorthButton);
-        myDirectionPanel.add(new JPanel());
-        myDirectionPanel.add(myWestButton);
-        myDirectionPanel.add(new JPanel());
-        myDirectionPanel.add(myEastButton);
-        myDirectionPanel.add(new JPanel());
-        myDirectionPanel.add(mySouthButton);
-        myDirectionPanel.add(new JPanel());
-
-        controls.add(myDirectionPanel, BorderLayout.SOUTH);
-
-
-
-//        myFrame.add(myMapPanel);
-
-        // Right Half
-        // Top Right
-        myQuestionAnswerPanel = new JPanel(new BorderLayout());
-
-        myQuestion = new JTextArea();
-        myQuestion.setLineWrap(true);
-        myQuestion.setText("Cat's Paw heels, manufactured by Biltrite and first available in 1904, are instantly recognizable on vintage footwear due to their bright white no-slip rubber dots and asymmetrical semi-circles along the edges of the heels creating the Cat's Paw shape. Even more recognizable is this iconic American brand's bright red packaging for the heels that featured a black cat on the side.");
-        // TODO Get the question corresponding to selected door and display it
-
-        myQuestionAnswerPanel.add(myQuestion, BorderLayout.CENTER);
-
-        // Bottom Right
-        final String[] answerArray = {"Augustus Ludlow", "Shinki Hikaku", "John Lofgren", "Wabash"};
-
-        int numberOfAnswers = answerArray.length; // TODO get the number of answers from controller
-        myAnswerPanel = new JPanel(new GridLayout(numberOfAnswers + 2, 1));
-        myAnswerButtons = new JRadioButton[numberOfAnswers];
-        myAnswerButtonsGroup = new ButtonGroup();
-        for (int i = 0; i < numberOfAnswers; i++) {
-            myAnswerButtons[i] = new JRadioButton(answerArray[i]);
-            myAnswerButtonsGroup.add(myAnswerButtons[i]);
-        }
-        for (int i = 0; i < numberOfAnswers; i++) {
-            myAnswerPanel.add(myAnswerButtons[i]);
-        }
-        myAnswerPanel.add(new JButton("Submit"));
-        JPanel buffer = new JPanel();
-        myAnswerPanel.add(buffer);
-        myQuestionAnswerPanel.add(myAnswerPanel, BorderLayout.SOUTH);
-
-
-        controls.add(myQuestionAnswerPanel, BorderLayout.CENTER);
-        myFrame.add(controls, BorderLayout.EAST);
-
-
-
-        myFrame.setVisible(true);
+        return myMapDisplay;
     }
 
     public static void main(String[] args) {
