c86726b3803d126618210772c47e237f9d553adf
Anthony Nguyen <anguyenq@uw.edu>
2022-06-06 02:00:17 -0700

    Cleaned up imports

--- src/view/Difficulty.java
+++ src/view/Difficulty.java
@@ -7,9 +7,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Objects;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static view.AppTheme.*;
 
--- src/view/Start.java
+++ src/view/Start.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Objects;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 import static view.AppTheme.*;
