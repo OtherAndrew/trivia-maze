289d3c9738c3bcbd5a1836599e2e366b50700c00
Andrew Nguyen <andrewnguyen841@gmail.com>
2022-06-10 15:18:45 -0700

    complete answer test

--- src/tests/model/questions/AnswerTest.java
+++ src/tests/model/questions/AnswerTest.java
@@ -10,9 +10,15 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AnswerTest {
 
+    private String myTextTrue, myTextFalse;
+    private Answer myTrueAnswer, myFalseAnswer;
+
     @BeforeEach
     void setUp() {
-
+        myTextTrue = "test true";
+        myTextFalse = "test false";
+        myTrueAnswer = new Answer(myTextTrue, true);
+        myFalseAnswer = new Answer(myTextFalse, false);
     }
 
     @AfterEach
@@ -21,9 +27,13 @@ class AnswerTest {
 
     @Test
     void isCorrect() {
+        assertTrue(myTrueAnswer.isCorrect());
+        assertFalse(myFalseAnswer.isCorrect());
     }
 
     @Test
     void testToString() {
+        assertEquals(myTextTrue, myTrueAnswer.toString());
+        assertEquals(myTextFalse, myFalseAnswer.toString());
     }
 }
