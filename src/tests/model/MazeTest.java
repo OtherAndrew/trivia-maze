3c4a40b9c35c586948da0f543740222b1fff405f
Andrew Nguyen <andrewnguyen841@gmail.com>
2022-06-10 17:57:08 -0700

    partial implementation of MazeTest

--- src/tests/model/MazeTest.java
+++ src/tests/model/MazeTest.java
@@ -1,5 +1,7 @@
 package model;
 
+import controller.TriviaMaze;
+import model.mazecomponents.State;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,8 +10,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MazeTest {
 
+    private Maze myTestMaze;
+
     @BeforeEach
     void setUp() {
+        myTestMaze = new Maze(new TriviaMaze(), 4, 4);
     }
 
     @AfterEach
@@ -38,22 +43,48 @@ class MazeTest {
 
     @Test
     void getGoalLocation() {
+        int playerRow = myTestMaze.getPlayerLocation().getRow();
+        int playerCol = myTestMaze.getPlayerLocation().getCol();
+        int goalRow = myTestMaze.getGoalLocation().getRow();
+        int goalCol = myTestMaze.getGoalLocation().getCol();
+        assertTrue(goalRow == 0 || goalRow == 3
+                || goalCol == 0 || goalCol == 3);
+        assertTrue(Math.abs(playerRow - goalRow) >= 2
+            || Math.abs(playerCol - goalCol) >= 2);
     }
 
     @Test
     void getPlayerLocation() {
+        int playerRow = myTestMaze.getPlayerLocation().getRow();
+        int playerCol = myTestMaze.getPlayerLocation().getCol();
+        int goalRow = myTestMaze.getGoalLocation().getRow();
+        int goalCol = myTestMaze.getGoalLocation().getCol();
+        assertTrue(playerRow == 0 || playerRow == 3
+                || playerCol == 0 || playerCol == 3);
+        assertTrue(Math.abs(playerRow - playerCol) >= 2
+                || Math.abs(playerCol - goalCol) >= 2);
     }
 
     @Test
     void getRoomVisitedNum() {
+        assertEquals(1, myTestMaze.getRoomVisitedNum(true));
+        assertEquals(15, myTestMaze.getRoomVisitedNum(false));
     }
 
     @Test
     void getDoorStateNum() {
+        setAllDoors();
     }
 
     @Test
     void setAllDoors() {
+        myTestMaze.setAllDoors(State.UNDISCOVERED);
+        int numDoors = myTestMaze.getDoorStateNum(State.UNDISCOVERED);
+        assertEquals(numDoors, myTestMaze.getDoorStateNum(State.UNDISCOVERED));
+        for (State s : State.values()) {
+            myTestMaze.setAllDoors(s);
+            assertEquals(numDoors, myTestMaze.getDoorStateNum(s));
+        }
     }
 
     @Test
