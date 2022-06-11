49d8ee451951c8e7b4a118eeb9e2946033a4ee8c
Anthony Nguyen <anguyenq@uw.edu>
2022-06-10 17:02:44 -0700

    Finished HashMapDisjointSetTest

--- src/tests/model/HashMapDisjointSetTest.java
+++ src/tests/model/HashMapDisjointSetTest.java
@@ -1,30 +1,54 @@
 package model;
 
+import model.mazecomponents.Room;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HashMapDisjointSetTest {
 
-    @BeforeEach
-    void setUp() {
+    private Random myRand;
+    private Room[][] myRooms;
+    private HashMapDisjointSet myDis;
+
+    private Room getRandomRoom() {
+        return myRooms[myRand.nextInt(myRooms.length)][myRand.nextInt(myRooms[0].length)];
     }
 
-    @AfterEach
-    void tearDown() {
+    @BeforeEach
+    void setUp() {
+        myRand = new Random();
+        myRooms = new Room[myRand.nextInt(7) + 4][myRand.nextInt(7) + 4];
+        for (int row = 0; row < myRooms.length; row++) {
+            for (int col = 0; col < myRooms[row].length; col++) {
+                myRooms[row][col] = new Room(row, col);
+            }
+        }
+        myDis = new HashMapDisjointSet(myRooms);
     }
 
     @Test
     void find() {
+        final Room room = getRandomRoom();
+        assertSame(room, myDis.find(room));
     }
 
     @Test
     void join() {
+        final Room roomA = getRandomRoom();
+        final Room roomB = getRandomRoom();
+        myDis.join(roomA, roomB);
+        assertEquals(myRooms.length * myRooms[0].length - 1, myDis.getSize());
+        assertSame(roomB, myDis.find(roomA));
     }
 
     @Test
     void getSize() {
+        assertEquals(myRooms.length * myRooms[0].length, myDis.getSize());
     }
 }
